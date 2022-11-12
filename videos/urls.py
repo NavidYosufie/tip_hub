8d1021a11ba6e86f661470f3bcb8118c3578411b
navid <navid.yosufie4@gmail.com>
2022-11-12 05:43:18 -0800

    add search

--- videos/urls.py
+++ videos/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("videos", views.all_videos, name="all_videos"),
     path("category/<slug:slug>", views.category_detail, name="cat_det"),
     path("detail/<slug:slug>", views.video_detail, name="detail"),
+    path("search/", views.search_videos, name="search")
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Video, Category_Detail
+from .models import Video, Category_Detail, Comment
 from django.core.paginator import Paginator
 
 def all_videos(request):
@@ -12,6 +12,10 @@ def all_videos(request):
 
 def video_detail(request, slug):
     videos_detail = get_object_or_404(Video, slug=slug)
+    if request.method == 'POST':
+        body = request.POST.get("body")
+        parent_id = request.POST.get("parent_id")
+        Comment.objects.create(body=body, videos=videos_detail, user=request.user, parent_id=parent_id)
     return render(request, "video-detail.html", {"videos": videos_detail})
 
 
@@ -19,3 +23,11 @@ def category_detail(request, slug):
     cat_det = get_object_or_404(Category_Detail, slug=slug)
     videos = cat_det.video_set.all()
     return render(request, "all-videos.html", {"videos": videos})
+
+def search_videos(request):
+    search = request.GET.get("search")
+    videos = Video.objects.filter(title__icontains=search)
+    get_number_page = request.GET.get("page")
+    paginator = Paginator(videos, 1)
+    object_list = paginator.get_page(get_number_page)
+    return render(request, "all-videos.html", {"videos": object_list})
